5134e260f0386841d3f2810c18de63d33bdc04bf
alexshere <86909156+alexshere@users.noreply.github.com>
2021-07-06 23:43:45 +0700

    Update santet.py

--- userbot/modules/santet.py
+++ userbot/modules/santet.py
@@ -8,9 +8,9 @@ async def typewriter(typew):
     typew.pattern_match.group(1)
     await typew.edit(f"`{ALIVE_NAME} Berusaha Menyantet Jamet Ini`")
     sleep(2)
-    await typew.edit("`Mencari Nama Jamet Ini...`")
+    await typew.edit("`Mencari Identitas Jamet Ini...`")
     sleep(1)
-    await typew.edit("`Santet Online Segera Dilakukan,Mampus Lu!!`")
+    await typew.edit("`Santet Online Otw, Jan Salahin Gw Kalo Hp Lo NgeFreeze, Hang, Lag, Ato Ketutup Sendiri Tod!`")
     sleep(1)
     await typew.edit("0%")
     number = 1
@@ -313,7 +313,7 @@ async def typewriter(typew):
     sleep(0.03)
     await typew.edit(str(number) + "%   ████████████████▌")
     sleep(1)
-    await typew.edit("`Target Berhasil Tersantet Online:v`")
+    await typew.edit("`Ga jadi nyantet, lo miskin, jin gw ga tega liat lu menderita`")
 
 
 CMD_HELP.update({
